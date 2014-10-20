903be163a57cf45a0adb54c222f26cec36f21a76
Luming <lumingnie@gmail.com>
2014-10-20 15:41:03 -0400

    modified vlab_demo/settings.py

--- vlab_demo/settings.py
+++ vlab_demo/settings.py
@@ -57,8 +57,15 @@ WSGI_APPLICATION = 'vlab_demo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+        # authentication:
+        # username: root password: 123
     }
 }
 
