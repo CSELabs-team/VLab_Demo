5797f446567461fe1f3ebeeac4c06b5f05706243
Luming <lumingnie@gmail.com>
2014-10-23 16:59:34 -0400

    Added Static files for front-end developing, and modified logout redirection

---
+++ apps/thirdauth/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, include, url
+
+
+urlpatterns = patterns('',
+            # url(r'^logout/$', 'django.contrib.auth.views.logout',
+            #           {'next_page': '/successfully_logged_out/'})
+)
--- apps/vlab_panel/views.py
+++ apps/vlab_panel/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render_to_response, redirect
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.contrib.auth.models import User
+from django.contrib.auth import logout
 
 def panel_index(request):
     # authentication:
