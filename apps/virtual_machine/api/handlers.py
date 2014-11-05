6885b3a1257d35d73b1e6ceeef1ead95ee9dc4e5
Luming <lumingnie@gmail.com>
2014-11-05 17:56:30 -0500

    added api structure for waking up virtual machine

---
+++ apps/virtual_machine/api/handlers.py
@@ -0,0 +1,15 @@
+from rest_framework.views import APIView
+
+class WakeUpVirtualMachine(APIView):
+    """
+    Class based API for Wake Up virtual machine.
+    """
+
+    def post(self, request):
+        """
+        POST method for processing the Virtual Machine Wakeup request
+        """
+        try:
+
+        except Exception:
+
--- apps/virtual_machine/views.py
+++ apps/virtual_machine/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Creating Views for dealing with the operations on Virtual Machine
+# APIs are implemented in another folder - api
+# In this View controller, the panel calls class based view functions and then these functions call API in the api folder
+# to wake up virtual machines
