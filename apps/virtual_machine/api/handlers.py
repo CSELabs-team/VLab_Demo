590b0b71e026f8c0f68724bdb73b938331310443
Luming <lumingnie@gmail.com>
2014-11-06 17:31:28 -0500

    Added VM API structure and modified views.

--- apps/virtual_machine/api/handlers.py
+++ apps/virtual_machine/api/handlers.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
+import logging
 
-class WakeUpVirtualMachine(APIView):
+class WakeUpVMHandler(APIView):
     """
     Class based API for Wake Up virtual machine.
     """
@@ -10,6 +11,26 @@ class WakeUpVirtualMachine(APIView):
         POST method for processing the Virtual Machine Wakeup request
         """
         try:
+            logging.debug("Waking up the Virtual Machine with POST request.")
+            # Call back end script to wake up virtual machine
 
         except Exception:
+            logging.error("Failed to wake up the Virtual Machine.")
 
+
+class VMStatusHandler(APIView):
+
+    """
+    APIs for getting Virtual Machine status.
+    """
+
+    def get(self, request):
+        """
+        GET method for processing the Virtual Machine Wakeup request
+        """
+        try:
+            logging.debug("Getting Virtual Machine status with GET request.")
+            # Call back end script to wake up virtual machine
+
+        except Exception:
+            logging.error("Failed to get the Virtual Machine Status.")
--- apps/vlab_panel/urls.py
+++ apps/vlab_panel/urls.py
@@ -4,12 +4,4 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^$', views.panel_index, name='panel_index'),
-
-    # # url(r'^social_login$', views.social_login, name='social_login'),
-    # url(r'^add$', views.add_to_do, name='add_item'),
-    # url(r'^delete$', views.delete_item, name='delete_item'),
-    # url(r'^edit$', views.edit_item, name='edit_item'),
-    # # url('', include('social.apps.django_app.urls', namespace='social')),
-   	# url('', include('django.contrib.auth.urls', namespace='auth')),
-   	# url(r'^admin/', include(admin.site.urls)),
 )
